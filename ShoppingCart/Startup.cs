a2f7415bd2b0b7943f2998dc64b7d9d5acdbf71c
David <devido5555@gmail.com>
2021-06-11 17:27:12 +0200

    Admin Index method

--- ShoppingCart/Startup.cs
+++ ShoppingCart/Startup.cs
@@ -53,6 +53,10 @@ namespace ShoppingCart
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+             );
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
