51e54c3bd133a7dc8de2bee9f4581dec491c2d72
David <devido5555@gmail.com>
2021-07-06 19:52:26 +0200

    Admin POST, CSRF protection, disable home delete

--- ShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ ShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -42,5 +42,33 @@ namespace ShoppingCart.Areas.Admin.Controllers
         // GET admin/pages/create
         public IActionResult Create() => View();
 
+        // POST /admin/pages/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Page page)
+        {
+            if(ModelState.IsValid)
+            {
+                page.Slug = page.Title.ToLower().Replace(" ", "-");
+                page.Sorting = 100;
+
+                var slug = await context.Pages.FirstOrDefaultAsync(x => x.Slug == page.Slug);
+                if(slug != null)
+                {
+                    ModelState.AddModelError("", "The title already exists.");
+                    return View(page);
+                }
+
+                context.Add(page);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The page has been added!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(page);
+        }
+
     }
 }
