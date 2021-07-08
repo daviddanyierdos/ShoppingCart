51eec195d86e066d448e487668774cbd84921f9a
David <devido5555@gmail.com>
2021-07-08 17:51:47 +0200

    Admin Categories controller, model and migration

---
+++ ShoppingCart/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        readonly ShoppingCartContext context;
+        public CategoriesController(ShoppingCartContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
