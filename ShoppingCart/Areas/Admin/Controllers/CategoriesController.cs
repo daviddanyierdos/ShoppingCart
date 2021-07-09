1f5fd2d8fba802fb484a4fc73ada281fa20bb3a0
David <devido5555@gmail.com>
2021-07-09 13:26:11 +0200

    Admin Categories Create

--- ShoppingCart/Areas/Admin/Controllers/CategoriesController.cs
+++ ShoppingCart/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShoppingCart.Infrastructure;
+using ShoppingCart.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +17,42 @@ namespace ShoppingCart.Areas.Admin.Controllers
         {
             this.context = context;
         }
+
+        // GET /admin/categories
         public async Task<IActionResult> Index()
         {
             return View(await context.Categories.OrderBy(x => x.Sorting).ToListAsync());
+        }
+
+        // GET /admin/categories/create
+        public IActionResult Create() => View();
+
+        // POST admin/categories/create
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.Slug = category.Name.ToLower().Replace(" ", "-");
+                category.Sorting = 100;
+
+                var slug = await context.Categories.FirstOrDefaultAsync(x => x.Slug == category.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The category already exists.");
+                    return View(category);
+                }
+
+                context.Add(category);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The category has been added!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
         }
     }
 }
